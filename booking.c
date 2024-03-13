a650fd6dd77c7b78f24092a5a9f24c5e7578872d
Marco Wong <58595299+junonnwong@users.noreply.github.com>
2024-03-14 02:20:48 +0800

    MW

--- booking.c
+++ booking.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include<time.h>
 #pragma warning(disable:4996)
 
+void showTime(void) {
+	system("cls");
+	time_t t;
+	time(&t);
+	printf("%110s", ctime(&t));
+}
+
 int main(void) {
-	printf("hello,world");
+	showTime();
 }
