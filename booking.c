b8114dd37416f82c0fafb7eee9fbeb166fb3a46b
Wong Jun Onn <wongjo-wm23@student.tarc.edu.my>
2024-03-15 23:55:24 +0800

    MW

--- booking.c
+++ booking.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<time.h>
 #include<string.h>
+#include<math.h>
 #pragma warning(disable:4996)
 #define MAX_BOOKING 10
 
@@ -73,28 +74,63 @@ void createDate(Info userChoice[MAX_BOOKING]) {
 }
 
 int calcFare(Info userChoice[MAX_BOOKING]) {
-	double totalPrice[10];
+	double totalPrice[10], sumPrice = 0, adj = 0, negative;
+	int sumPriceint;
 	title(NULL);
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 110; i++) {
 		printf("_");
 	}
 	printf("\n");
-	printf("| %-10s| %-13s| %-16s| %-14s| %-5s| %-15s| %-13s|\n", "Date", "Destination", "Departure Time", "Arrival Time", "Pax", "Price per Pax", "Total Price");
+	printf("| %-10s| %-13s| %-16s| %-14s| %-5s| %-20s| %-18s|\n", "Date", "Destination", "Departure Time", "Arrival Time", "Pax", "Price per Pax (RM)", "Total Price (RM)");
 	for (int j = 0; j < 10; j++) {
 		if (userChoice[j].pax != NULL) {
 			totalPrice[j] = userChoice[j].price * (double)userChoice[j].pax;
-			printf("| %-10s| %-13s| %-16d| %-14d| %-5d| %-15.2f| %-13.2f|\n", date[j], userChoice[j].destination, userChoice[j].prefer.time.depart, userChoice[j].prefer.time.arrive, userChoice[j].pax, userChoice[j].price, totalPrice[j]);
+			printf("| %-10s| %-13s| %-16d| %-14d| %-5d| %-20.02f| %-18.02f|\n", date[j], userChoice[j].destination, userChoice[j].prefer.time.depart, userChoice[j].prefer.time.arrive, userChoice[j].pax, userChoice[j].price, totalPrice[j]);
+			sumPrice += totalPrice[j];
 		}
 	}
-	for (int i = 0; i < 100; i++) {
-	printf("-");
+	for (int i = 0; i < 110; i++) {
+		printf("-");
 	}
+	printf("\n");
+	printf("%97s : RM %.02f\n", "Total", sumPrice);
+	if (ceil(sumPrice) != floor(sumPrice)) {
+		sumPriceint = sumPrice * 100;
+		sumPriceint %= 10;
+		switch (sumPriceint) {
+		case 1:
+		case 2:
+			adj = -((double)sumPriceint / 100);
+			break;
+		case 3:
+		case 4:
+			adj = 0.05 - ((double)sumPriceint / 100);
+			break;
+		case 6:
+		case 7:
+			adj = -(((double)sumPriceint / 100) - 0.05);
+			break;
+		case 8:
+		case 9:
+			adj = 0.1 - ((double)sumPriceint / 100);
+			break;
+		}
+	}
+	if (adj < 0) {
+		negative = adj * -1;
+		printf("%97s : -RM %.02f\n", "Rounding Adjustment", negative);
+	}
+	else {
+		printf("%97s : RM %.02f\n", "Rounding Adjustment", adj);
+	}
+	sumPrice += adj;
+	printf("%97s : RM %.02f\n", "Total Price should payment", sumPrice);
 }
 
 int main(void) {
 	Info userChoice[MAX_BOOKING] = { "Kampar", 1, 1, 4, 2024, 8, 11 };
 	reset(userChoice);
-	userChoice[0] = (Info) { "Kampar", 1, 50, { 1, 4, 2024,{ 8, 11} } };
+	userChoice[0] = (Info) { "Kampar", 1, 50.26, { 1, 4, 2024,{ 8, 11} } };
 	title(NULL);
 	createDate(userChoice);
 	calcFare(userChoice);
