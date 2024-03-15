6bb3a59de46ffa4a553184131dcc31c6d9c6c6c0
lgw123456789 <leeguanwei007@gmail.com>
2024-03-15 20:38:32 +0800

    GW

--- member.c
+++ member.c
@@ -2,21 +2,36 @@
 #include <stdlib.h>
 #include <time.h>
 #pragma warning(disable:4996)
+#define memberName 100
+#define memberPass 100
+#define maxMember 100 
+
 
 void memberMenu();
 void menuLogin();
 void memberLogin();
 void memberRegister();
 void forgotPass();
+void memberMainPage();
+
+struct Member {
+	char id[memberName];
+	char pass[memberPass];
+	
+};
 
 main() {
-	memberMenu();
+	struct Member member[maxMember];
+	system("cls");
+	memberMenu(member);
 
 }
 
-void menuLogin(){
+void memberMenu(struct Member* member) {
 	char *menu[] = {"Login", "Registration", "Forgot Password", "Exit Program"};
 	int choice;
+	printf("Member Menu\n");
+	printf("-----------\n");
 	for (int i = 0; i < sizeof(menu)/sizeof(menu[0]); i++) {
 		printf("-----\n");
 		printf("| %d | %s\n", i + 1, menu[i]);
@@ -27,7 +42,7 @@ void menuLogin(){
 	do {
 		switch (choice) {
 		case 1: 
-			memberLogin();
+			memberLogin(member);
 			break;
 		case 2:
 			memberRegister();
@@ -42,13 +57,27 @@ void menuLogin(){
 	} while (choice !=4);
 }
 
-void memberMenu(){
-	printf("Member Menu\n");
-	printf("-----------\n");
-	menuLogin();
-}
+//void memberMenu(){
+//	printf("Member Menu\n");
+//	printf("-----------\n");
+//	menuLogin();
+//}
 
-void memberLogin() {
+void memberLogin(struct Member* member) {
+	char name[memberName], password[memberPass];
+	
+	system("cls");
+	printf("Name: ");
+	scanf(" %[^\n]", name);
+	printf("Password: ");
+	scanf(" %[^\n]", password);
+	
+	for (int i = 0; i < maxMember; i++) {
+		if (strcmp(member[i].id, name) == 0 && strcmp(member[i].pass, password) == 0) {
+			printf("Login successful...\n");
+
+		}
+	}
 
 
 }
@@ -58,3 +87,5 @@ void memberRegister() {
 void forgotPass() {
 
 }
+
+void memberMainPage();
