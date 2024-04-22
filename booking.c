d397380127a741f2fd602e67a5ae3a83bf6f9a07
Wong Jun Onn <145476852+TARUMT-JunOnn@users.noreply.github.com>
2024-04-22 17:44:17 +0800

    MW

--- booking.c
+++ booking.c
@@ -82,26 +82,6 @@ void reset(Info *userChoice) {
 }
 
 //Same with all module
-void dayOfWeek(int numOfWeek, char(*dayReturn)[10]) {
-	const char day[7][10] = { {"Sunday"} , {"Monday"} ,{"Tuesday"} ,{"Wednesday"} ,{"Thursday"} ,{"Friday"} ,{"Saturday"} };
-	strcpy((*dayReturn), day[numOfWeek]);
-}
-
-void title(void) {
-	char day[10];
-	system("cls");
-	printf("%10s %s %s", "Train", "Ticketing", "System");
-	GetLocalTime(&t);
-	dayOfWeek(t.wDayOfWeek, &day);
-	const char month[12][10] = { {"January"}, {"February"}, {"March"}, {"April"}, {"May"}, {"June"}, {"July"}, {"August"}, {"September"}, {"October"}, {"November"}, {"December"}};
-	printf("%104s %02d %s %d %02d:%02d:%02d", day, t.wDay, month[t.wMonth - 1], t.wYear, t.wHour, t.wMinute, t.wSecond);
-	printf("\n");
-	for (int i = 0; i < 155; i++) {
-		printf("%s", "-");
-	}
-	printf("\n\n");
-}
-
 int readfile(FILE** fptr) {
 	int i = 0;
 	*fptr = fopen("../TrainTicketingSys/res/booking.txt", "r");
